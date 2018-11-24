58947a1f9f806e6721a9f4112149b8f106f4b2f3
author <jk5309@naver.com>
2018-11-24 14:36:12 +0000

    login_add

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,67 +1,55 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Login extends Component{
     constructor(props){
         super(props);
         this.state = {
-            ServerAddr: "http://ec2-52-32-190-25.us-west-2.compute.amazonaws.com:3001",
-            checkId:'',
-            checkPassword:'',
-            data: [
-                {
-                    id:'',
-                    password:'',
-                    email: '',
-                    role: '',}
-            ],
-        };
+		id:'',
+		password:''
+	}
     }
 
-    componentDidMount() {
-        axios.get(this.state.ServerAddr+'/member')
-            .then( res => {this.setState({
-                data: res.data
-                })
-            })
-            .catch( res => { console.log("error");
-            });
+    login = (e) => {
+	e.preventDefault();
+	console.log(this.state)
+	let ServerAddr = 'http://ec2-52-32-190-25.us-west-2.compute.amazonaws.com:4001'   
+        axios.post(ServerAddr+'/login',this.state)
+        .then( res => {
+		console.log(res.data.success)
+		if(res.data.success){
+			sessionStorage.setItem('dtoken',res.data.token);
+			console.log(sessionStorage.getItem('dtoken'));
+		/*여기에 로그인 성공시메인으로 자동  화면 전환 해야하는 코드 추가 할 것*/
+		}
+	})
+        .catch( res => { 
+		console.log(res);
+        });
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
+    handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
         });
-        this.setState({
-            checkId: '',
-            checkPassword: ''
-        })
     }
-    /*
-    check = () =>{
-        if(!this.state.Id.filter( id => {})) {
-          //
-        };
-    }*/
     render(){
-        console.log(this.state.data);
         return(
-            <div>
-            <Form inline onSubmit={this.handleSubmit}>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                    <Label for="ID" className="mr-sm-2"><h2>ID</h2></Label>
-                    <Input type="text" name="checkId"/>
-                </FormGroup>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                    <Label for="Password" className="mr-sm-2"><h2>Password</h2></Label>
-                    <Input type="password" name="checkPassword"/>
-                </FormGroup>
-                <Button type = "submit">Sign in</Button>
-            </Form>
-            </div>
-        );
+      	<Container>
+	<Form onSubmit={this.login}>
+        <FormGroup>
+          <Label for="id">Id</Label>
+          <Input type="text" name="id" id="id" placeholder="id" onChange={this.handleChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" onChange={this.handleChange} />
+        </FormGroup>
+	<Button>로그인</Button>
+	</Form>
+	</Container>
+	);
     }
 }
 
